fb1e90016fcf3f62b5d9e5d00c7ca3a1ce59d77c
Mohamed <bazman.social@gmail.com>
2024-02-01 15:47:27 +0100

    polishing things up

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,86 @@
 import './App.css'
 import groceryCartImg from "./assets/pepe.gif"
 import { useState } from "react"
+
+
+
 function App() {
 
   const [inputvalue, setInputValue] = useState("");
+  const [groceryItems, setGroceryItems] = useState([]);
+
 
   const handleChangeInputValue = (e) =>{
     setInputValue(e.target.value);
+  };
+
+  const handleAddGroceryItem =(e) => {
+    if(e.key === 'Enter') {
+      if(inputvalue) {
+        const updatedGroceryList = [...groceryItems]  
+        const itemIndex = updatedGroceryList.findIndex(item => item.name === inputvalue);
+      if(itemIndex === -1) {
+        updatedGroceryList.push({
+          name: inputvalue,
+          quantity: 1,
+          completed: false
+        })
+      } else {
+        updatedGroceryList[itemIndex].quantity++
+      }
+
+      setGroceryItems(updatedGroceryList)
+      setInputValue("");
+      }
+    }
+  };
+
+  const handleRemoveItem = () => {
+
   }
+
+  const renderGroceryList = () => {
+    return groceryItems.map((item) => (
+      <li key={item.name}>
+          <div className='container'>
+            <input type="checkbox" />
+            <p>{
+            item.name} {item.quantity > 1 && <span>x{item.quantity}</span>}
+            </p>
+          </div>
+          <div>
+            <button 
+            className='remove-button'
+            onClick={handleRemoveItem}
+            >X
+            </button>
+          </div>
+        </li>
+    )); 
+
+  };
+  
+
   return (
     <main className="App">
       <div>
       <div>
         <h4 className='success'>You're Done</h4>
         <div className='header'>
-          {inputvalue}
-          <button onClick={() =>{
-            setInputValue("");
-          }}>
-            Clear Input
-          </button>
           <h1>Shopping List</h1>
           <img src={groceryCartImg} alt="" />
-          <input type="text" 
-          placeholder='Add an item' 
-          className='item-input' 
-          onChange={handleChangeInputValue}
-          value={inputvalue}
+          <input 
+            type="text" 
+            placeholder='Add an item' 
+            className='item-input' 
+            onChange={handleChangeInputValue}
+            onKeyDown={handleAddGroceryItem}
+            value={inputvalue}
           />
         </div>
       </div>
       <ul>
-        <li>
-          <div className='container'>
-            <input type="checkbox" />
-            <p>Carrots</p>
-          </div>
-          <div>
-            <button className='remove-button'>X</button>
-          </div>
-        </li>
+        {renderGroceryList()}
       </ul>
       </div>
     </main>
